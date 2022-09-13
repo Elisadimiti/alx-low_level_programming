a7efbf0cd6bbd4d7a144d7d04bc4308d98cb85d9
Elisa Hezikiah Dimiti <elisadimiti5@gmail.com>
2022-09-13 15:07:44 -0700

    Functions

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,6 +2,8 @@
 /**
  * Function that prints the alphabet, in lowercase
  *
+ * Description: prints alphabet using _putchar
+ * main.h: created header
  * Return: None
  **/
 void print_alphabet(void)
